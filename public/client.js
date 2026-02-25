89a8312b12da27c26df59e528a635a6583117be9
Mtellez93 <ing.miguelangeltellez@gmail.com>
2026-02-25 15:27:44 -0800

    Create client.js

---
+++ public/client.js
@@ -0,0 +1,41 @@
+const socket = io();
+
+function getPlayerId() {
+  let id = localStorage.getItem("playerId");
+  if (!id) {
+    id = crypto.randomUUID();
+    localStorage.setItem("playerId", id);
+  }
+  return id;
+}
+
+function joinGame() {
+  const name = document.getElementById("nameInput").value;
+
+  socket.emit("join-game", {
+    playerId: getPlayerId(),
+    name
+  });
+}
+
+function startGame() {
+  socket.emit("start-game", {});
+}
+
+function nextLetter() {
+  socket.emit("next-letter");
+}
+
+socket.on("update-state", state => {
+  document.getElementById("letter").innerText =
+    state.currentLetter || "-";
+
+  const playersList = document.getElementById("players");
+  playersList.innerHTML = "";
+
+  state.players.forEach(player => {
+    const li = document.createElement("li");
+    li.innerText = `${player.name} - ${player.score} pts ${player.connected ? "" : "(Desconectado)"}`;
+    playersList.appendChild(li);
+  });
+});
