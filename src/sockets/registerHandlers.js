a9cc78c9af7fea134c98e909d3ed768f9e7758d1
Mtellez93 <ing.miguelangeltellez@gmail.com>
2026-02-25 15:33:11 -0800

    Update registerHandlers.js

--- src/sockets/registerHandlers.js
+++ src/sockets/registerHandlers.js
@@ -5,44 +5,39 @@ function registerHandlers(io, socket, game) {
       socketId: socket.id,
       name
     });
-
     io.emit("update-state", game.getState());
   });
 
   socket.on("start-game", () => {
-    const playerId = game.getPlayerIdBySocket(socket.id);
-    if (playerId !== game.hostId) return;
+    const pid = game.getPlayerIdBySocket(socket.id);
+    if (pid !== game.hostId) return;
 
     game.startGame();
     io.emit("update-state", game.getState());
   });
 
-  socket.on("next-letter", () => {
-    const playerId = game.getPlayerIdBySocket(socket.id);
-    if (playerId !== game.hostId) return;
-
-    game.nextLetter();
-    io.emit("update-state", game.getState());
-  });
-
   socket.on("submit-answers", answers => {
-    const playerId = game.getPlayerIdBySocket(socket.id);
-    game.submitAnswers(playerId, answers);
-
+    const pid = game.getPlayerIdBySocket(socket.id);
+    game.submitAnswers(pid, answers);
     io.emit("update-state", game.getState());
   });
 
-  socket.on("host-decision", ({ playerId, approved }) => {
-    const senderId = game.getPlayerIdBySocket(socket.id);
-    if (senderId !== game.hostId) return;
+  socket.on("start-voting", () => {
+    const pid = game.getPlayerIdBySocket(socket.id);
+    if (pid !== game.hostId) return;
 
-    game.hostDecision(playerId, approved);
+    game.startVoting();
     io.emit("update-state", game.getState());
+  });
+
+  socket.on("vote", ({ targetId, category, approve }) => {
+    const voterId = game.getPlayerIdBySocket(socket.id);
+    game.vote(voterId, targetId, category, approve);
   });
 
   socket.on("finalize-round", () => {
-    const senderId = game.getPlayerIdBySocket(socket.id);
-    if (senderId !== game.hostId) return;
+    const pid = game.getPlayerIdBySocket(socket.id);
+    if (pid !== game.hostId) return;
 
     game.finalizeRound();
     io.emit("update-state", game.getState());
